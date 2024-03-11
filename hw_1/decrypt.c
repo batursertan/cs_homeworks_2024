1f3ce0c33be7d612acfb69e5fe0c1ba6c260fbad
batursertan <sertan.batur.estgn@gmail.com>
2024-03-11 14:16:55 +0300

    hw_1 is finished just gonna change the names before upload

--- hw_1/decrypt.c
+++ hw_1/decrypt.c
@@ -1,23 +1,54 @@
-#include<stdio.h>
+#include <stdio.h>
+
+char alphabet[61] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '(', '<', '=', '+', ')', '[', '*', '/', ']', '{', '>', '!', '-', '}', '?', '\\', '&', '|', '%', '_', ';', '"', '#', '.', '\'', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+
+void caesar_decipher(char *text, int key) {
+    while (*text) {
+        int i;
+        for (i = 0; i < 61; i++) {
+            if (*text == alphabet[i]) {
+                *text = alphabet[(i - key + 61) % 61];
+                break;
+            }
+        }     
+        text++;
+    }
+}
 
 int main() {
 
-    char alphabet[60] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
-    'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '(', '<', '=', '+', ')',
-    '[', '*', '/', ']', '{', '>', '!', '-', '}', '?', '\\', '&', '|', '%', '_', ';', '"', '#',
-    '\'', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+    char c;
+    int key = 7;
+    int comment_len;
+    int in_comm = 0;
 
-    char str[200];
-    int i, len = 0;
-    
-    printf("enter the code:\n");
-    scanf("%s", str);
+    FILE *input = fopen("encrypted.txt", "r");
+    FILE *output = fopen("decrypted.txt", "w");
 
-    char str[200];
-    for(i=0; str[i]!= "\0"; i++){
-        len++;
+    if (input == NULL) {
+        perror("Couldn't open the encrypted.txt");
+        return -1;
     }
 
+    while ((c = fgetc(input)) != EOF) {
 
+        if (c == '@') {
+            fscanf(input, "%d", &comment_len); 
+            fprintf(output, "<Comment with length %d characters>", comment_len);
+            in_comm = 1; 
+        } else if (!in_comm && c == '\n') { /* making sure to print the new line characters before the decypher*/
+            fputc(c, output);
+        } else if (!in_comm) { 
+            caesar_decipher(&c, key);
+            fputc(c, output);
+        } else if (in_comm && c == '\n') { /* same new line logic before but inside the comment*/
+            in_comm = 0; 
+            fputc(c, output);
+        }
+    }
+
+    fclose(input);
+    fclose(output);
+
+    return 0;
 }
-
--- hw_1/encrypt.c
+++ hw_1/encrypt.c
@@ -1,22 +1,60 @@
-#include<stdio.h>
+#include <stdio.h>
+
+char alphabet[61] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '(', '<', '=', '+', ')', '[', '*', '/', ']', '{', '>', '!', '-', '}', '?', '\\', '&', '|', '%', '_', ';', '"', '#', '.', '\'', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+
+void caesar_cipher(char *text, int key) {
+    while (*text) {  
+        int i;
+        for (i = 0; i < 61; i++) {
+            if (*text == alphabet[i]) {
+                *text = alphabet[(i + key) % 61]; 
+                break;
+            }
+        }  
+        text++;
+    }
+}
 
 int main() {
 
-    char alphabet[60] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
-    'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '(', '<', '=', '+', ')',
-    '[', '*', '/', ']', '{', '>', '!', '-', '}', '?', '\\', '&', '|', '%', '_', ';', '"', '#',
-    '\'', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+    char c;
+    int key = 7;  /*student id: 220104004849*/
+    int comment_len;
 
-    char str[200];
-    int i, len = 0;
-    
-    printf("enter the code:\n");
-    scanf("%s", str);
+    FILE *input = fopen("input.txt", "r");
+    FILE *output = fopen("encrypted.txt", "w");
 
-    char str[200];
-    for(i=0; str[i]!= "\0"; i++){
-        len++;
+    if (input == NULL) {
+        perror("Couldn't open the input.txt");
+        return -1;
+    }
+
+    while ((c = fgetc(input)) != EOF) {
+
+        if (c == '/') {
+            if ((c = fgetc(input)) == '*'){ /*making sure we are inside the comment*/
+                comment_len = 0;
+
+                while ((c = fgetc(input)) && c != EOF && !(c == '*' && (c = fgetc(input)) == '/')) { /*maybe writing a still in comment func would be better*/
+                    comment_len++;
+                }
+                fprintf(output, "@ %d", comment_len);
+
+            } else {
+                caesar_cipher(&c, key);
+                fputc(c, output);
+            }
+
+        } else {
+            caesar_cipher(&c, key);
+            fputc(c, output);
+        }
     }
 
 
+    fclose(input);
+    fclose(output);
+
+    return 0;
 }
+
